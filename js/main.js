13cd0690cad30f96f496f3972efc44637f5cde0f
Rory Duncan <robust.rory@gmail.com>
2013-12-24 18:16:44 -0800

    Add #selfie, hero slide.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+$(document).ready(
+  (function() {
+
+      var h = $(window).height(),
+          w = $(window).width();
+
+      slide = $('.slide');
+      nav = $('div.header-container');
+      
+      h = h - nav.innerHeight();
+
+      // put the content 10% from the top
+      slide.css({
+        "min-height": (h) + "px",
+        "padding-top": (h * 0.10) + "px",
+
+      });
+
+    
+  })
+);
