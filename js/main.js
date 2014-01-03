d74d7eb47e0cccd6305d06201bcecdecacb65c26
Rory Duncan <robust.rory@gmail.com>
2014-01-03 12:07:40 -0800

    Add toggle text to work slide. removed 'the future' from the last li's data-year attr.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ $(document).ready(
           w = $(window).width(),
           toggleActive = function(e) {
           	$(e.currentTarget).toggleClass("active");
+          },
+          expandAll = function() {
+          	$(".work ul.expandable li").removeClass("active");
+      		$(".work ul.expandable li").addClass("active");
+      		$(".work p a").addClass("active");
+      		$(".work p a").text("Collapse All  [-]");
+          },
+          collapseAll = function() {
+          	$(".work ul.expandable li").removeClass("active");
+          	$(".work p a").removeClass("active");
+      		$(".work p a").text("Expand All  [+]");
           };
       	
 
@@ -22,7 +33,8 @@ $(document).ready(
       });
 
 
-
+      // Expansion of work list items when clicked
+      // required for touch interfaces
       $(".work ul.expandable li").on("click", toggleActive);
       $("nav .menu").on("click", function(e) {
       	if ( $("nav").hasClass("active") ) {
@@ -40,6 +52,18 @@ $(document).ready(
       	// 'active'-ate the menu that was clicked.
       	toggleActive(e);
       });
-    
-  })
-);
+
+      
+      // expand or collapse all work items
+      $(".toggle-all").on("click", function(e) {
+      	e.preventDefault();
+      	if ( $(e.currentTarget).hasClass("active")) {
+      		collapseAll();
+      	}
+    	else expandAll();
+      });
+
+
+
+
+    }));
