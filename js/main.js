8701f7c807b8fd0d4d4c9806033038e04da006a7
Rory Duncan <robust.rory@gmail.com>
2014-01-03 16:33:35 -0800

    Add fancy scrolling and simple slide tracking.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 
 
 $(document).ready(
-  (function() {
+	(function() {
+
 
       var h = $(window).height(),
           w = $(window).width(),
@@ -19,12 +20,31 @@ $(document).ready(
           	$(".work p a").removeClass("active");
       		$(".work p a").text("Expand All  [+]");
           };
+
+          if (w >= 485) {
+            $("div.next-slide").hide();
+            $(".hero").children().hide();
+
+
+            window.setTimeout( function() {
+            $(".hero h1").fadeIn(600);
+            }, 300)
+
+            window.setTimeout( function() {
+              $(".hero p").fadeIn(600);
+            }, 1000)
+
+            window.setTimeout( function() {
+              $("div.next-slide").fadeIn(600);
+            }, 2000)
+          }
       	
 
       slide = $('.slide');
       nav = $('div.header-container');
       
       h = h - nav.innerHeight();
+      window.slideHeight = h;
 
       // put the content 10% from the top
       slide.css({
@@ -33,7 +53,8 @@ $(document).ready(
       });
 
 
-      // Expansion of work list items when clicked
+      /* * Expansion of work list items when clicked * */
+
       // required for touch interfaces
       $(".work ul.expandable li").on("click", toggleActive);
       $("nav .menu").on("click", function(e) {
@@ -54,7 +75,8 @@ $(document).ready(
       });
 
       
-      // expand or collapse all work items
+      /* * expand or collapse all work items * */
+
       $(".toggle-all").on("click", function(e) {
       	e.preventDefault();
       	if ( $(e.currentTarget).hasClass("active")) {
@@ -63,7 +85,76 @@ $(document).ready(
     	else expandAll();
       });
 
+/* * SCROLLSPY-like * */
+	//helpers
+	var flatten = function(number) {return (~~number)};
+	var isWithin$ = function(value, $sel) {
+
+		if (value >= $sel.scrollTop() && value <= ($sel.offset().top + $sel.height() ) ) {
+			return true;
+		}
+		else return false;
+	};
+
+	var getScrollTop = function() {
+		return $(document).scrollTop();
+	};
+
+	/* * CORE FUNCTIONING * */
+
+	// specific positions:
+	var epoch = $("#epoch"),
+		work = $("#work"), 
+		skills = $("#skills"), 
+		about = $("#about"), 
+		contact = $("#contact"),
+
+		currentSlide = null,
+		nextSlide = $("#work"),
+		currentSlideIndex = 0,
+		listed = [epoch, work, skills, about, contact];
+
+	var getCurrentSlide = function() {
+
+		var scroll = getScrollTop();
+
+		// test each jQuery object to determine which slide to assign to currentSlide variable
+		for (var i = 0, ii = listed.length; i < ii; i+=1) {
+			if ( isWithin$(scroll, listed[i]) === true ) {
+				currentSlide = listed[i];
+				currentSlideIndex = i;
+				console.log("current slide is ", currentSlide.selector)
+				break;
+			}
+			else continue;
+		}
+	}
+	var toggleNextSlide = function() {
+		if (currentSlideIndex === ( listed.length - 2 ) ){
+			$("div.next-slide").slideUp();
+		}
+		else $("div.next-slide").slideDown();
+	};
 
 
 
-    }));
+
+    $('body').on("mousewheel", function() {
+    	getCurrentSlide();
+		toggleNextSlide();
+    });
+
+
+      
+/* * Scroll-button * */
+      $(".next-slide").on("click", function() {
+
+      	//update what is considered the current slide
+      	getCurrentSlide();
+      	// handler for if it's the last slide
+      	toggleNextSlide();
+      	// scroll time
+      	$.scrollTo( listed[currentSlideIndex+1], 1000);
+      	
+      });
+}));
